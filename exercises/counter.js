89185b53a034d44babe4c5015a47c8b43b229f36
heather Reeve <heather.reeve@scope.org.uk>
2024-06-24 11:28:44 +0100

    completed

--- exercises/counter.js
+++ exercises/counter.js
@@ -1,7 +1,11 @@
 // Make your Counter class here
-
+class Counter{
+    constructor (Counter) {
+        this.count = 0
+    }
+}
 // Replace `null` with an _instance_ of your counter
-const counterInstance = null
+const counterInstance =  new Counter ()
 
 // Please don't change the lines below!
 export { Counter, counterInstance }
