06641e7e1cc5070d690b3a38e21fe688b7125580
heather Reeve <heather.reeve@scope.org.uk>
2024-06-27 11:02:28 +0100

    error message

---
+++ tests/maths.test.js
@@ -0,0 +1,12 @@
+import Maths from '../exercises'
+import { describe, it} from 'mocha'
+import assert from 'assert'
+
+describe('Maths class', function() {
+    it('should have PI equal to 3.14', function (){
+        const PI = Maths.PI (
+            assert.strictEqual (PI, 3.14)
+        )
+    })
+})
+
