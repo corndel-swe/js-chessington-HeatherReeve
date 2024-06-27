520a92078ca83fd2e3854c191f266f06cddee5f7
heather Reeve <heather.reeve@scope.org.uk>
2024-06-27 15:54:24 +0100

    rectangles done

--- tests/rectangles.test.js
+++ tests/rectangles.test.js
@@ -8,5 +8,13 @@ describe('Area of rectangle', function() {
   it('should call the area method', function() {
     const area = newRectangle.area()
     assert.strictEqual(area, 50)
+  
   })
+})
+describe('Perimeter of rectangle', function() {
+    const newRectangle = new Rectangle(5, 10)
+  it ('should call the perimeter', function () {
+        const perimeter = newRectangle.perimeter ()
+        assert.strictEqual (perimeter, 30)
+    })
 })
